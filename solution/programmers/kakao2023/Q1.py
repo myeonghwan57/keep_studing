d1a9fe70a838d782297b02f953689b6deb0f3f25
myeonghwna57 <myeonghwan57@gmail.com>
2023-04-03 21:56:01 +0900

    프로그래머스 코딩 테스트 연습

---
+++ solution/programmers/kakao2023/Q1.py
@@ -0,0 +1,11 @@
+#고객의 약관 동의 1~n 번으로 분류되는 개인정보 n 유효기간 있음 
+# A-6 / B-12/ c-3 
+#today 문자열 유효기간 terms 배열 privacies 문자열 
+# def solution(today, terms, privacies):
+#     answer = []
+#     for i in range(len(privacies)):
+
+#     return answer
+
+str = '2023.04.03 A'
+print(str.split()[1])
