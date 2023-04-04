e0625253d04fd4a1871d8ef049bade2cb46c09f9
myeonghwna57 <myeonghwan57@gmail.com>
2023-04-04 21:20:49 +0900

    코딩테스트 연습

--- solution/programmers/kakao2023/Q1.py
+++ solution/programmers/kakao2023/Q1.py
@@ -1,11 +1,22 @@
 #고객의 약관 동의 1~n 번으로 분류되는 개인정보 n 유효기간 있음 
 # A-6 / B-12/ c-3 
 #today 문자열 유효기간 terms 배열 privacies 문자열 
-# def solution(today, terms, privacies):
-#     answer = []
-#     for i in range(len(privacies)):
+def dateToDay(date):
+    year, month, day = map(int, date.split("."))
+    return (year * 12 * 28) + (month * 28) + day
+def solution(today, terms, privacies):
+    answer = []
 
-#     return answer
+    today = dateToDay(today)
 
-str = '2023.04.03 A'
-print(str.split()[1])
+    termsInfo = dict()
+    for term in terms:
+        term = term.split()
+        termsInfo[term[0]] = int(term[1]) * 28
+    
+    for i in range(len(privacies)):
+        date, term = privacies[i].split()
+        if dateToDay(date) + termsInfo[term] <= today:
+            answer.append(i+1)
+        
+    return answer
