80d1e89e3b5bd1f1b2d8140bea83336185254ef5
myeonghwna57 <myeonghwan57@gmail.com>
2023-04-10 17:41:14 +0900

    카카오 코딩테스트 Q3

--- solution/programmers/kakao2023/Q3.py
+++ solution/programmers/kakao2023/Q3.py
@@ -1,20 +1,39 @@
 #n명의 사용자 M개 판매 
-# 할인율은 10 20 30 40 이모티콘 마다 할인율 다름.
+# 할인율은 10 20 30 40 이모티콘 마다 할인율 다름. 각각의 할인율로 정해짐 
 # 이모티콘 모두 구매, 자신이 정한 일정 할인율 이상이 되면 모두 구매 
 # 자신이 생각하는 가격 이상이 되면 구독함
-
+from itertools import product
 def solution(users, emoticons):
-    answer = [0,0]
-    for user in users:
-        ratio, price = user.split()
-        all_price = 0
-        for emoticon in emoticons :
-            emo_ratio, emo_price = emoticon.split()
-            if emo_ratio >= ratio:
-                all_price += emo_price*(100-emo_ratio) // 100
-        if price > all_price:
-            answer[1]+=all_price
-        else:
-            answer[0] += 1
+    subscriber_cnt = 0
+    max_price = 0
+    dataset = product([10,20,30,40],repeat=len(emoticons))
+    for data in dataset:
+        user_cnt = 0
+        sum_price = 0
+        for user in users:
+            total = 0
+            for i in range(len(emoticons)):
+                if data[i] >= user[0]:
+                    total += (100-data[i])*emoticons[i]/100
+            if total >= user[1]:
+                user_cnt += 1
+            else:
+                sum_price += total
+        
+        if subscriber_cnt < user_cnt:
+            subscriber_cnt = user_cnt
+            max_price = sum_price
+        elif subscriber_cnt == user_cnt:
+            if max_price < sum_price:
+                max_price = sum_price
+    return [subscriber_cnt,max_price]
+        
+
+
     
-    return answer
+print(solution([[40,10000],[25,10000]],[7000,9000]))
+
+# dataset =  [10,20,30,40]
+# saledata = list(product(dataset,repeat = 2))
+
+# print(saledata[1][1])
