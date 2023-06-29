e7866283bd9d031135947b14d38a41cfc7a9254d
myeonghwna57 <myeonghwan57@gmail.com>
2023-06-29 17:57:00 +0900

    프로그래머스 숫자게임 문제풀이

---
+++ solution/programmers/coding_test/Level3/solution_숫자게임.py
@@ -0,0 +1,12 @@
+def solution(A, B):
+    answer = 0
+    A.sort()
+    B.sort()
+    j = 0
+
+    for i in range(len(A)):
+        if A[j] < B[i]:
+            answer = answer + 1
+            j = j+1
+
+    return answer
