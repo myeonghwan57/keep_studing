9f442d9e4046d7f94e849fde101f89c1fb50e457
myeonghwna57 <myeonghwan57@gmail.com>
2023-07-13 22:25:15 +0900

    문제풀이 수정

--- solution/programmers/coding_test/Level2/solution_과제진행하기.py
+++ solution/programmers/coding_test/Level2/solution_과제진행하기.py
@@ -1,39 +1,9 @@
-from collections import deque
+# 과제를 받은 루는 다음과 같은 순서대로 과제를 하려고 계획을 세웠습니다.
+# 과제는 시작하기로 한 시각이 되면 시작합니다.
+# 새로운 과제를 시작할 시각이 되었을 때, 기존에 진행 중이던 과제가 있다면 진행 중이던 과제를 멈추고 새로운 과제를 시작합니다.
+# 진행중이던 과제를 끝냈을 때, 잠시 멈춘 과제가 있다면, 멈춰둔 과제를 이어서 진행합니다.
+# 만약, 과제를 끝낸 시각에 새로 시작해야 되는 과제와 잠시 멈춰둔 과제가 모두 있다면, 새로 시작해야 하는 과제부터 진행합니다.
+# 멈춰둔 과제가 여러 개일 경우, 가장 최근에 멈춘 과제부터 시작합니다.
+# 과제 계획을 담은 이차원 문자열 배열 plans가 매개변수로 주어질 때, 
+# 과제를 끝낸 순서대로 이름을 배열에 담아 return 하는 solution 함수를 완성해주세요.
 
-def convert_time(s) :
-    h, m = map(int, s.split(':'))
-    return h*60 + m
-
-def solution(plans):
-    answer = []
-    
-    plans = [ (name, convert_time(start), int(playtime)) for name, start, playtime in plans]
-    plans.sort(key = lambda x : x[1])
-    
-    assign_q = deque()
-    left_time = 0
-    
-    for i in range(len(plans)) :
-        name, start, playtime = plans[i]
-        
-        while assign_q :
-            _name, _playtime = assign_q.pop()
-            if left_time >= _playtime :
-                left_time -= _playtime
-                answer.append(_name)
-            else :
-                assign_q.append((_name, _playtime - left_time))
-                break
-                
-        assign_q.append((name, playtime))
-        
-        if i < len(plans)-1 :
-            next_start = plans[i+1][1]
-            left_time = next_start - start
-        
-    while assign_q :
-        _name, _ = assign_q.pop()
-        answer.append(_name)
-        
-    
-    return answer
