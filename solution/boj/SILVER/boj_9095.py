77066679ddf66a2ab86870f53c9a18a88ceee283
myeonghwna57 <myeonghwan57@gmail.com>
2023-08-25 14:57:29 +0900

    백준 9095 문제풀이

---
+++ solution/boj/SILVER/boj_9095.py
@@ -0,0 +1,25 @@
+# 정수 4를 1, 2, 3의 합으로 나타내는 방법은 총 7가지가 있다. 합을 나타낼 때는 수를 1개 이상 사용해야 한다.
+
+# 1+1+1+1
+# 1+1+2
+# 1+2+1
+# 2+1+1
+# 2+2
+# 1+3
+# 3+1
+# 정수 n이 주어졌을 때, n을 1, 2, 3의 합으로 나타내는 방법의 수를 구하는 프로그램을 작성하시오.
+
+import sys
+read = sys.stdin.readline
+
+cache = [0] * 11
+cache[1] = 1
+cache[2] = 2
+cache[3] = 4
+
+for i in range(4, 11):
+    cache[i] = sum(cache[i-3:i])
+
+T = int(read())
+for _ in range(T):
+    print(cache[int(read())])
